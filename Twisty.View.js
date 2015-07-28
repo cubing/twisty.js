62a9021167f4dbd4212bc94c286b9b7f1d7f548c
Lucas Garron <code@garron.net>
2015-07-28 00:47:30 -0700

    Clean up Javascript formatting.

--- Twisty.View.js
+++ Twisty.View.js
@@ -1,164 +1,164 @@
 // if (!Twisty) {
 //   Twisty = {};
 // }
-
 // var Twisty.View = function() {}
-      var camera, scene, renderer;
-      var controls;
 
-      var objects = [];
-      var targets = { table: [], sphere: [], helix: [], grid: [] };
+var camera, scene, renderer;
+var controls;
 
-      init();
-      animate();
+var objects = [];
+var targets = {
+    table: [],
+    sphere: [],
+    helix: [],
+    grid: []
+};
 
-      function init() {
+init();
+animate();
 
-        var dimension = 3;
+function init() {
 
-        var scale = 120;
-        zoom = 100;
+    var dimension = 3;
 
-        var sides = function() {
-          var h = dimension / 2;
+    var scale = 120;
+    zoom = 100;
+    scene = new THREE.Scene();
 
-          var x = new THREE.Vector3(h, 0, 0);
-          var y = new THREE.Vector3(h, 0, 0);
-          var z = new THREE.Vector3(h, 0, 0);
+    var sides = function() {
+        var h = dimension / 2;
 
-          var x_ = new THREE.Vector3().negate(x);
-          var y_ = new THREE.Vector3().negate(y);
-          var z_ = new THREE.Vector3().negate(z);
+        var x = new THREE.Vector3(h, 0, 0);
+        var y = new THREE.Vector3(h, 0, 0);
+        var z = new THREE.Vector3(h, 0, 0);
 
-          var sides = {
-            U: {face: new THREE.Matrix4().lookAt(z , z_, y )},
-            L: {face: new THREE.Matrix4().lookAt(x_, x , z )},
-            F: {face: new THREE.Matrix4().lookAt(y_, y , z )},
-            R: {face: new THREE.Matrix4().lookAt(x , x_, z )},
-            B: {face: new THREE.Matrix4().lookAt(y , y_, z )},
-            D: {face: new THREE.Matrix4().lookAt(z_, z , y_)},
-          }
+        var x_ = new THREE.Vector3().negate(x);
+        var y_ = new THREE.Vector3().negate(y);
+        var z_ = new THREE.Vector3().negate(z);
 
-          return sides;
-        }();
-
-        scene = new THREE.Scene();
-
-        for (s in sides) {
-          var side = sides[s];
-
-          for (var i = 0; i < dimension; i++) {
-            for (var j = 0; j < dimension; j++) {
-              var element = document.createElement( 'div' );
-              element.classList.add("sticker", s);
-              element.style.backgroundPosition = "" + ((i) * 100) + "% " + ((j) * 100) + "%";
-              var object = new THREE.CSS3DObject(element);
-
-              var element2 = document.createElement( 'div' );
-              element2.classList.add("cubie", s);
-              element2.style.backgroundPosition = "" + ((i) * 100) + "% " + ((j) * 100) + "%";
-              var object2 = new THREE.CSS3DObject(element2);
-
-              object.matrixAutoUpdate = false;
-              object2.matrixAutoUpdate = false;
-
-
-var m1 = new THREE.Matrix4();
-console.log(s);
-switch (s) {
-  case "U": m1.makeRotationX(-Math.PI/2); break;
-  case "L": m1.makeRotationY(-Math.PI/2); break;
-  case "F": m1.makeRotationZ(0); break;
-  case "R": m1.makeRotationY(Math.PI/2); break;
-  case "B": m1.makeRotationY(Math.PI); break;
-  case "D": m1.makeRotationX(Math.PI/2); break;
-}
-
-
-var m2 = new THREE.Matrix4()
-m2.makeTranslation((i + (1 - dimension)/2) * scale, (j + (1 - dimension)/2) * scale, dimension / 2 * scale + 0.01);
-
-object.matrix.scale(new THREE.Vector3(1/dimension, 1/dimension, 1/dimension));
-object.matrix.multiply( m1 );
-object.matrix.multiply( m2 );
-
-var m22 = new THREE.Matrix4()
-m22.makeTranslation((i + (1 - dimension)/2) * scale, (j + (1 - dimension)/2) * scale, dimension / 2 * scale - 1);
-
-object2.matrix.scale(new THREE.Vector3(1/dimension, 1/dimension, 1/dimension));
-object2.matrix.multiply( m1 );
-object2.matrix.multiply( m22 );
-
-// console.log(object.matrix);
-              // object.matrix.multiply(side.face);
-              // object.matrix.multiply(
-              //  new THREE.Matrix4().set(
-              //    1, 0, 0, 0,
-              //    0, 1, 0, 0,
-              //    0, 0, 1, 0,
-              //    0, 0, 0, 1
-              //  )
-              // );
-
-              // console.log(object.matrix);
-
-              scene.add(object);
-              // scene.add(object2);
-            }
-          }
+        var sides = {
+            U: {face: new THREE.Matrix4().lookAt(z, z_, y)},
+            L: {face: new THREE.Matrix4().lookAt(x_, x, z)},
+            F: {face: new THREE.Matrix4().lookAt(y_, y, z)},
+            R: {face: new THREE.Matrix4().lookAt(x, x_, z)},
+            B: {face: new THREE.Matrix4().lookAt(y, y_, z)},
+            D: {face: new THREE.Matrix4().lookAt(z_, z, y_)},
         }
 
+        return sides;
+    }();
 
-        camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
-        camera.position.x = zoom;
-        camera.position.y = zoom;
-        camera.position.z = zoom;
-        camera.lookAt(new THREE.Vector3(0, 0, -4000));
-        camera.updateProjectionMatrix();
+    for (s in sides) {
+        var side = sides[s];
 
-        // table
+        for (var i = 0; i < dimension; i++) {
+            for (var j = 0; j < dimension; j++) {
+                var element = document.createElement('div');
+                element.classList.add("sticker", s);
+                element.style.backgroundPosition = "" + ((i) * 100) + "% " + ((j) * 100) + "%";
+                var object = new THREE.CSS3DObject(element);
+
+                var element2 = document.createElement('div');
+                element2.classList.add("cubie", s);
+                element2.style.backgroundPosition = "" + ((i) * 100) + "% " + ((j) * 100) + "%";
+                var object2 = new THREE.CSS3DObject(element2);
+
+                object.matrixAutoUpdate = false;
+                object2.matrixAutoUpdate = false;
 
 
-        renderer = new THREE.CSS3DRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.domElement.style.position = 'absolute';
-        document.getElementById( 'container' ).appendChild( renderer.domElement );
+                var m1 = new THREE.Matrix4();
+                console.log(s);
+                switch (s) {
+                    case "U":
+                        m1.makeRotationX(-Math.PI / 2);
+                        break;
+                    case "L":
+                        m1.makeRotationY(-Math.PI / 2);
+                        break;
+                    case "F":
+                        m1.makeRotationZ(0);
+                        break;
+                    case "R":
+                        m1.makeRotationY(Math.PI / 2);
+                        break;
+                    case "B":
+                        m1.makeRotationY(Math.PI);
+                        break;
+                    case "D":
+                        m1.makeRotationX(Math.PI / 2);
+                        break;
+                }
 
-        //
 
-        controls = new THREE.TrackballControls( camera, renderer.domElement );
-        controls.rotateSpeed = 6;
-        controls.minDistance = 500;
-        controls.maxDistance = 6000;
-        controls.addEventListener( 'change', render );
+                var m2 = new THREE.Matrix4()
+                m2.makeTranslation((i + (1 - dimension) / 2) * scale, (j + (1 - dimension) / 2) * scale, dimension / 2 * scale + 0.01);
 
-        render()
+                object.matrix.scale(new THREE.Vector3(1 / dimension, 1 / dimension, 1 / dimension));
+                object.matrix.multiply(m1);
+                object.matrix.multiply(m2);
 
-        window.addEventListener( 'resize', onWindowResize, false );
+                var m22 = new THREE.Matrix4()
+                m22.makeTranslation((i + (1 - dimension) / 2) * scale, (j + (1 - dimension) / 2) * scale, dimension / 2 * scale - 1);
 
-      }
+                object2.matrix.scale(new THREE.Vector3(1 / dimension, 1 / dimension, 1 / dimension));
+                object2.matrix.multiply(m1);
+                object2.matrix.multiply(m22);
 
-      function onWindowResize() {
+                // console.log(object.matrix);
+                // object.matrix.multiply(side.face);
+                // object.matrix.multiply(
+                //  new THREE.Matrix4().set(
+                //    1, 0, 0, 0,
+                //    0, 1, 0, 0,
+                //    0, 0, 1, 0,
+                //    0, 0, 0, 1
+                //  )
+                // );
 
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+                // console.log(object.matrix);
 
-        renderer.setSize( window.innerWidth, window.innerHeight );
+                scene.add(object);
+                scene.add(object2);
+            }
+        }
+    }
 
-        render();
 
-      }
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.x = zoom;
+    camera.position.y = zoom;
+    camera.position.z = zoom;
+    camera.lookAt(new THREE.Vector3(0, 0, -1));
+    camera.updateProjectionMatrix();
 
-      function animate() {
+    renderer = new THREE.CSS3DRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('container').appendChild(renderer.domElement);
 
-        requestAnimationFrame( animate );
+    controls = new THREE.TrackballControls(camera, renderer.domElement);
+    controls.rotateSpeed = 6;
+    controls.minDistance = 500;
+    controls.maxDistance = 6000;
+    controls.addEventListener('change', render);
 
-        controls.update();
+    render();
 
-      }
+    window.addEventListener('resize', onWindowResize, false);
+}
 
-      function render() {
+function render() {
+    renderer.render(scene, camera);
+}
 
-        renderer.render( scene, camera );
+function onWindowResize() {
+    // camera.aspect = window.innerWidth / window.innerHeight; // Not needed?
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    render();
+}
 
-      }
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+}
